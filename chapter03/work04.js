520d01b58f509bdfb7d4769aaea62a31f5736b3d
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2022-01-12 09:11:38 +0000

    added chapter03

---
+++ chapter03/work04.js
@@ -0,0 +1,21 @@
+// メソッドの戻り値
+// クラス定義
+class Triangle {
+    // コンストラクタ
+    constructor(base, height) {
+        this.base = base;
+        this.height = height;
+    }
+    
+    // メソッド定義
+    getArea() {
+        const area = (this.base * this.height) / 2;
+        return area;
+    }
+}
+
+// オブジェクト作成
+const triangle = new Triangle(12, 3);
+// メソッド呼び出し
+const area = triangle.getArea();
+console.log("三角形の面積：" + area);
