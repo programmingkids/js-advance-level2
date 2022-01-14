1a3975bba41830136cd318fc8cc05dffd8b13c25
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2022-01-14 07:30:06 +0000

    fix chapter02 small comments

--- chapter02/work03.js
+++ chapter02/work03.js
@@ -1,5 +1,4 @@
 // 即時実行関数
-
 // 無名関数の宣言と実行を同時に行う
 (function(){
     // Hello, John
@@ -7,7 +6,7 @@
     console.log(`Hello, ${name}`);
 })();
 
-// 無名関数の宣言と実行を同時に行う
+// 無名関数の宣言と実行を同時に行う 2個の引数を渡す
 (function(num1, num2){
     // 15
     console.log( num1 * num2 );
